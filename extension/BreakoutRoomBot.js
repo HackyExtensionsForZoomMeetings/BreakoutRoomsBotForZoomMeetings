c9bb4f71b76ca9eda6d85bfa9d62194650deef8d
Nelson Chen <crazysim@gmail.com>
2020-09-10 20:12:29 -0700

    Join the chatbox send together

--- extension/BreakoutRoomBot.js
+++ extension/BreakoutRoomBot.js
@@ -337,8 +337,8 @@ var moveRequestFulfillNotifySubscription = moveRequestErrorsAndSuccess$.subscrib
         }
         try {
             assignedUnjoinedUserToBreakoutRoom(sender, roomName);
-            chatboxSend(`🎯 (from ${src})\n Assigning\n "${sender}"\n to\n "${roomName}"\n`)
-            chatboxSend("❓ You may need to press the Breakout Rooms button\n to join the newly assigned breakout meeting.\n❓ Chat \"!ls\" to list rooms and other commands.\n");
+            chatboxSend(`🎯 (from ${src})\n Assigning\n "${sender}"\n to\n "${roomName}"\n`+
+            "❓ You may need to press the Breakout Rooms button\n to join the newly assigned breakout meeting.\n❓ Chat \"!ls\" to list rooms and other commands.\n");
         } catch {
 
         }
